08303eabbb883a47cc7d7f533dbb3123417b322f
marcelo10 <marcelo79@gmail.com>
2017-03-30 17:44:58 -0500

    housekeeping

--- server.js
+++ server.js
@@ -1,29 +1,21 @@
 require('dotenv').config();
 
-const express = require('express');
+var express = require('express');
 var bodyParser = require('body-parser')
-const app = express();
-var  port = process.env.PORT || 3000;
-const router = express.Router();
-const pg = require('pg');
-const path = require('path');
-const connectionString = process.env.DATABASE_URL ;
+var pg = require('pg');
+
+const port = process.env.PORT;
+const connectionString = process.env.DATABASE_URL;
 const localApiKey = process.env.API_KEY;
+
+const app = express();
+const router = express.Router();
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
-//app.use(function (req, res, next) {
-//
-//    if (req.header('apikey') == localApiKey) {
-//        next();
-//    }
-//    else {
-//        res.json({message: 'invalid key'});
-//    }
-//});
 
 app.get('/v1/quotes/random', (req, res, next) => {
   const results = [];
@@ -115,41 +107,41 @@ app.post('/v1/quotes', (req, res, next) => {
 });
 
 app.put('/v1/quotes/:quote_id', (req, res, next) => {
-if (req.header('apikey') == localApiKey)
-     {
-      const results = [];
-      // Grab data from the URL parameters
-      const id = req.params.quote_id;
-      // Grab data from http request
-      const data = {quote: req.body.quote};
-      // Get a Postgres client from the connection pool
-      pg.connect(connectionString, (err, client, done) => {
-        // Handle connection errors
-        if(err) {
-          done();
-          console.log(err);
-          return res.status(500).json({success: false, data: err});
-        }
-        // SQL Query > Update Data
-        client.query('UPDATE quotes SET quote=($1) WHERE id=($2)',
-        [data.quote, id]);
-        // SQL Query > Select Data
-        const query = client.query("SELECT * FROM quotes ORDER BY id ASC");
-        // Stream results back one row at a time
-        query.on('row', (row) => {
-          results.push(row);
+  if (req.header('apikey') == localApiKey)
+      {
+        const results = [];
+        // Grab data from the URL parameters
+        const id = req.params.quote_id;
+        // Grab data from http request
+        const data = {quote: req.body.quote};
+        // Get a Postgres client from the connection pool
+        pg.connect(connectionString, (err, client, done) => {
+          // Handle connection errors
+          if(err) {
+            done();
+            console.log(err);
+            return res.status(500).json({success: false, data: err});
+          }
+          // SQL Query > Update Data
+          client.query('UPDATE quotes SET quote=($1) WHERE id=($2)',
+          [data.quote, id]);
+          // SQL Query > Select Data
+          const query = client.query("SELECT * FROM quotes ORDER BY id ASC");
+          // Stream results back one row at a time
+          query.on('row', (row) => {
+            results.push(row);
+          });
+          // After all data is returned, close connection and return results
+          query.on('end', function() {
+            done();
+            return res.json(results);
+          });
         });
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-          done();
-          return res.json(results);
-        });
-      });
-     }
-    else 
-    {
-        res.json({message: 'invalid key'});
-    }     
+      }
+      else 
+      {
+          res.json({message: 'invalid key'});
+      }     
 });
 
 app.delete('/v1/quotes/:quote_id', (req, res, next) => {
